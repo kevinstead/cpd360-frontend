d08dae91bf39fdcab3989649d79b94b7e26b7176
Kevin Stead <dadkevin9@gmail.com>
2025-06-01 03:13:21 -0700

    Fix: Ensure react-scripts is included

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Welcome to CPD360</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
