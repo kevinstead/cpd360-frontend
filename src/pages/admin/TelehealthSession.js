ed7d876ed04f41b36893d641f2db6162a2958224
Kevin Stead <dadkevin9@gmail.com>
2025-06-13 15:07:30 -0700

    git push

--- src/pages/admin/TelehealthSession.js
+++ src/pages/admin/TelehealthSession.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import Layout from '../../layouts/Layout';
-import Video from '@twilio/video';
+import Video from 'twilio-video';
 import axios from 'axios';
 
 export default function TelehealthSession() {
